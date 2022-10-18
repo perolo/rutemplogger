87be2c1b782324f2b8bcfd012447f9c61709edbe
Per Olofsson <per.e.olofsson@gmail.com>
2022-10-18 16:27:49 +0200

    Simple loop reading all sensors every 30s

--- src/ds18b20.rs
+++ src/ds18b20.rs
@@ -3,26 +3,16 @@ use std::path::PathBuf;
 use crate::w1_errors::*;
 
 
-
 static W1_PATH_PREFIX: &str = "/sys/bus/w1/devices";
 static W1_PATH_SUFFIX: &str = "w1_slave";
 
 
-
 pub struct MilliCelsius(u32);
 impl MilliCelsius {
-/*     pub fn to_fahrenheit(self) -> f64 {
-        (self.0 as f64) / 1000.0 / 5.0 * 9.0 + 32.0
-    }*/
 
     pub fn to_celsius(self) -> f64 {
         (self.0 as f64) / 1000.0 
     }
-/*
-    pub fn as_u32(self) -> u32 {
-        self.0
-    }
-    */
 }
 
 
@@ -30,13 +20,8 @@ impl MilliCelsius {
 pub struct DS18B20 {
    pub w1_id: Vec<String>
 }
-/*
-impl DS18B20 {
-    pub fn get_id(self, index: usize) -> String {
-        self.w1_id[index]
-    }
-}
-*/
+
+
 impl DS18B20 {
     pub fn new() -> Result<DS18B20, io::Error> {
         let mut v : Vec<String> = Vec::new();  
@@ -53,13 +38,8 @@ impl DS18B20 {
         }
 panic!("Unable to find a DS18B20")
     }
-/*
-    pub fn new_for_id(id: String) -> DS18B20 {
-         DS18B20 {
-             w1_id: id
-         }
-    }
-*/
+
+    
     pub fn read_raw(&self, name: String) -> io::Result<String> {
         let mut path = PathBuf::from(W1_PATH_PREFIX);
         path.push(name);
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 mod ds18b20;
 mod w1_errors;
-//use std::env;
+use std::{thread, time};
 
 fn main() {
     let sensors = ds18b20::DS18B20::new().unwrap();
-    for sensor in &sensors.w1_id {
-        let s = String::from(sensor);
-        let temp = sensors.read_temp(s).unwrap();
-        println!("{} : {:.1} C", sensor, temp.to_celsius());
-    }
 
-    // Default print friendly, but allow "-r/--raw" to
-    // be passed to display the millicelsius directly instead
-    /* 
-    let args: Vec<String> = env::args().collect();
-    if (args.len() > 1) && args[1].contains("-r") {
-        println!("{}", temp.as_u32());
-    }
-    else {
+    let sleep_delay = time::Duration::from_secs(30);
+    let mut now = time::Instant::now();
+    loop {
         
+        for sensor in &sensors.w1_id {
+            let s = String::from(sensor);
+            let temp = sensors.read_temp(s).unwrap();
+            println!("{} : {:.1} C", sensor, temp.to_celsius());
+        }
+        let read_done = time::Instant::now();
+        now = now.checked_add(sleep_delay).unwrap();
+        let dur = now - read_done;
+        println!("Sleep {:#?} s",  dur );
+        thread::sleep(dur);
     }
-    */
 }
